31b853a861a8b52a6589e4cff7d0f06267ec91cd
Edzer Dionido <jeddionido26@gmail.com>
2025-03-28 00:36:20 +0800

    add string operations, incremental operations, and substring operations

--- store.js
+++ store.js
@@ -3,25 +3,109 @@
 // set, get, remove, has
 
 class InMemoryStore {
-    constructor() {
-        this.store = new Map();
+  constructor() {
+    this.store = new Map();
+  }
+
+  // CORE FUNCTIONS
+  set(key, value) {
+    this.store.set(key, value);
+  }
+
+  get(key) {
+    return this.store.get(key) || null;
+  }
+
+  remove(key) {
+    delete this.store.delete(key);
+  }
+
+  has(key) {
+    return this.store.has(key);
+  }
+
+  // STRING OPERATIONS
+  append(key, value) {
+    if (!this.store.has(key)) {
+      this.store.set(key, String(value));
+    } else {
+      this.store.set(key, this.store.get(key) + String(value));
     }
 
-    set(key, value) {
-        this.store.set(key, value);
+    return this.store.get(key).length;
+  }
+
+  strlen(key) {
+    return this.store.has(key) ? this.store.get(key).length : 0;
+  }
+
+  // NUMBER OPERATIONS
+  incr(key) {
+    if (!this.store.has(key)) {
+      this.store.set(key, "1");
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value)) throw new Error("Value is not an integer");
+      this.store.set(key, String(value + 1));
+    }
+    return this.store.get(key);
+  }
+
+  decr(key) {
+    if (!this.store.has(key)) {
+      this.store.set(key, "-1");
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value))
+        throw new Error("ERR value is not a number or out of range");
+      this.store.set(key, String(value - 1));
+    }
+    return this.store.get(key);
+  }
+
+  incrBy(key, valToIncrement) {
+    if (!this.store.has(key)) {
+      this.store.set(key, String(value));
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value)) throw new Error("Value is not an integer");
+      this.store.set(key, String(value + valToIncrement));
     }
 
-    get(key) {
-        return this.store.get(key) || null;
+    return this.store.get(key);
+  }
+
+  decBy(key, valToDecrement) {
+    if (!this.store.has(key)) {
+      this.store.set(key, String(value));
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value)) throw new Error("Value is not an integer");
+      this.store.set(key, String(value - valToDecrement));
     }
 
-    remove(key) {
-        delete this.store.delete(key);
+    return this.store.get(key);
+  }
+
+// SUBSTRING OPERATIONS
+
+  getRange(key, start, end) {
+    if (!this.store.has(key)) return null;
+    let value = this.store.get(key);
+    return value.substring(start, end + 1);
+  }
+
+  setRange(key, offset, substring) {
+    if(!this.store.has(key)) {
+        this.store.set(key, " ".repeat(offset) + substring);
+    } else {
+        let value = this.store.get(key);
+        let newVal = value.substring(0, offset) + substring + value.substring(offset + substring.length);
+        this.store.set(key, newVal);
     }
 
-    has(key) {
-        return this.store.has(key);
-    }
+    return this.store.get(key).length;
+  }
 }
 
 export default InMemoryStore;
