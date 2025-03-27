03fc9e7d08da829c140c81d5db5dcb33c9e23e2e
Edzer Dionido <jeddionido26@gmail.com>
2025-03-28 01:25:42 +0800

    rename store to db and add the string property to use the string methods

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 import InMemoryStore from "./store.js";
 
-const store = new InMemoryStore();
+const db = new InMemoryStore();
 
 // TEST CASES FOR STRINGS
 
-store.set("name", "John Doe");
+db.set("name", "John Doe");
 
-console.log("name exists: " + store.has("name")); // Output: true
-console.log("User's name is" + store.get("name")); // Output: "John Doe"
+console.log("name exists: " + db.has("name")); // Output: true
+console.log("User's name is " + db.get("name")); // Output: "John Doe"
 
-store.append("name", " Smith"); // Output: "John Doe Smith"
-console.log("User's new name is" + store.get("name"));
+db.string.append("name", " Smith"); // Output: "John Doe Smith"
+console.log("User's new name is " + db.get("name"));
 
-console.log(store.strlen("name") + " characters"); // Output: 13
+console.log(db.string.strlen("name") + " characters"); // Output: 14
 
 // TEST CASES FOR RANGES
-console.log(store.getRange("name", 0, 4)); // Output: "John"
-store.setRange("name", 9, " Dionido");
-console.log("User's new name is" + store.get("name"));
+console.log(db.string.getRange("name", 0, 4)); // Output: "John"
+
+db.string.setRange("name", 8, " Dionido");
+console.log("User's new name is " + db.get("name"));
 
 //  TEST CASES FOR NUMBERS
-store.set("age", 30);
-console.log("User's initial: " + store.get("age")); // Output: 30
+db.string.set("age", 30);
+console.log("User's initial: " + db.get("age")); // Output: 30
 
-store.incr("age");
-console.log("age increment: " + store.get("age")); // Output: 31
+db.string.incr("age");
+console.log("age increment: " + db.get("age")); // Output: 31
 
-store.decr("age");
-console.log("age decremented: " + store.get("age")); // Output: 30
+db.string.decr("age");
+console.log("age decremented: " + db.get("age")); // Output: 30
 
-store.incrBy("age", 5);
-console.log("age incremented by 5: " + store.get("age")); // Output: 35
+db.string.incrBy("age", 5);
+console.log("age incremented by 5: " + db.get("age")); // Output: 35
 
-store.decBy("age", 5);
-console.log("age decremented by 5: " + store.get("age")); // Output: 30
+db.string.decBy("age", 5);
+console.log("age decremented by 5: " + db.get("age")); // Output: 30
