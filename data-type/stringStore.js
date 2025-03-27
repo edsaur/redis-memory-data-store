bbc63528946f58d4601c7fc86c7143e564d2cdb1
Edzer Dionido <jeddionido26@gmail.com>
2025-03-28 01:13:05 +0800

    add separate store

---
+++ data-type/stringStore.js
@@ -0,0 +1,103 @@
+// Store all string methods here
+
+class StringStore {
+  constructor(store) {
+    this.store = store;
+  }
+
+  set(key, value) {
+    this.store.set(key, value);
+  }
+
+  get(key) {
+    return this.store.get(key) || null;
+  }
+
+  // STRING OPERATIONS
+  append(key, value) {
+    if (!this.store.has(key)) {
+      this.store.set(key, String(value));
+    } else {
+      this.store.set(key, this.store.get(key) + String(value));
+    }
+
+    return this.store.get(key).length;
+  }
+
+  strlen(key) {
+    return this.store.has(key) ? this.store.get(key).length : 0;
+  }
+
+  // NUMBER OPERATIONS
+  incr(key) {
+    if (!this.store.has(key)) {
+      this.store.set(key, "1");
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value)) throw new Error("Value is not an integer");
+      this.store.set(key, String(value + 1));
+    }
+    return this.store.get(key);
+  }
+
+  decr(key) {
+    if (!this.store.has(key)) {
+      this.store.set(key, "-1");
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value))
+        throw new Error("ERR value is not a number or out of range");
+      this.store.set(key, String(value - 1));
+    }
+    return this.store.get(key);
+  }
+
+  incrBy(key, valToIncrement) {
+    if (!this.store.has(key)) {
+      this.store.set(key, String(value));
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value)) throw new Error("Value is not an integer");
+      this.store.set(key, String(value + valToIncrement));
+    }
+
+    return this.store.get(key);
+  }
+
+  decBy(key, valToDecrement) {
+    if (!this.store.has(key)) {
+      this.store.set(key, String(value));
+    } else {
+      let value = Number(this.store.get(key));
+      if (isNaN(value)) throw new Error("Value is not an integer");
+      this.store.set(key, String(value - valToDecrement));
+    }
+
+    return this.store.get(key);
+  }
+
+  // SUBSTRING OPERATIONS
+
+  getRange(key, start, end) {
+    if (!this.store.has(key)) return null;
+    let value = this.store.get(key);
+    return value.substring(start, end + 1);
+  }
+
+  setRange(key, offset, substring) {
+    if (!this.store.has(key)) {
+      this.store.set(key, " ".repeat(offset) + substring);
+    } else {
+      let value = this.store.get(key);
+      let newVal =
+        value.substring(0, offset) +
+        substring +
+        value.substring(offset + substring.length);
+      this.store.set(key, newVal);
+    }
+
+    return this.store.get(key).length;
+  }
+}
+
+export default StringStore;
