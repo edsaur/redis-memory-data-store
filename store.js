4db04639873e9542a8cefaa5673e283e8acd6e76
Edzer Dionido <jeddionido26@gmail.com>
2025-03-27 23:16:02 +0800

    create a class with features to set, get, remove, andcheck if there's a key-value pair

---
+++ store.js
@@ -0,0 +1,27 @@
+// Create a class for key-value storage
+//  This will include the following methods:
+// set, get, remove, has
+
+class InMemoryStore {
+    constructor() {
+        this.store = new Map();
+    }
+
+    set(key, value) {
+        this.store.set(key, value);
+    }
+
+    get(key) {
+        return this.store.get(key) || null;
+    }
+
+    remove(key) {
+        delete this.store.delete(key);
+    }
+
+    has(key) {
+        return this.store.has(key);
+    }
+}
+
+export default InMemoryStore;
