ca3915a1fe516ed4848529eb30e5f5238c633294
Edzer Dionido <jeddionido26@gmail.com>
2025-03-28 17:52:24 +0800

    add separate script test for data types

---
+++ scriptTests/jsonScriptTests.js
@@ -0,0 +1,25 @@
+import db from "../db.js";
+
+// Test: JSON Store
+db.json.set("user:1", "$", { name: "John", age: 30 });
+console.log(db.json.get("user:1")); // { name: "John", age: 30 }
+db.json.set("user:1", "skills[0]", "JavaScript");
+console.log(db.json.get("user:1")); // { name: "John", age: 30, skills: ["JavaScript"] }
+
+
+db.json.set("user:2", "$", {
+  name: "John Doe",
+  age: 30,
+  skills: ["HTML", "CSS", "JavaScript"],
+});
+console.log(db.json.get("user:2")); // { name: "John", age: 30 }
+
+db.json.set("user:2", "skills[2]", "Node.js");
+console.log(`This gets the skills: ${JSON.stringify(db.json.get("user:2", "skills"))}`);
+
+db.json.set("user:2", "hobbies[0]", "Gaming"); // creates a new array if it doesn't exist
+console.log(db.json.get("user:2"));
+
+db.json.arrAppend("user:2", "skills", "Python"); // Should throw an error because path is not an array
+
+setInterval(() => db.saveSnapshot(), 10000);
---
+++ scriptTests/listsScriptTests.js
@@ -0,0 +1,14 @@
+import db from "../db.js";
+
+db.list.lpush("users", "Alice", "Bob", "Charlie");
+db.list.rpush("users", "David", "Eve");
+console.log(db.list.lrange("users", 0, -1));
+
+console.log(db.list.lpop("users")); // "Alice"
+console.log(db.list.rpop("users")); // "Eve"
+
+console.log(db.list.lrange("users", 0, -1));
+
+
+// Periodically save snapshot
+setInterval(() => db.saveSnapshot(), 10000);
